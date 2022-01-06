9ea0fbac6c6a2f792ba82f2e6875ddab9e9d4cb7
Oneya4 <46073127+Oneya4@users.noreply.github.com>
2022-01-06 23:39:00 +0300

    Update recipe_list.dart

--- lib/ui/recipes/recipe_list.dart
+++ lib/ui/recipes/recipe_list.dart
@@ -2,7 +2,6 @@ import 'dart:collection';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:chopper/chopper.dart';
 import 'package:provider/provider.dart';
